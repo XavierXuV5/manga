710cb02615351bf10b93b46029f8e54e0509e0ef
XuHang <1009378267@qq.com>
2022-07-05 12:50:30 +0800

    add: 增加测试

--- test.js
+++ test.js
@@ -3,31 +3,47 @@ const {config} = require("./config");
 const fs = require("fs");
 const {sleep} = require("./util");
 
-const start = async () => {
+const start = async (url) => {
+    let time = null;
     const browser = await (puppeteer.launch(config));
-
     const page = await browser.newPage();
-    await page.goto('https://www.maofly.com/manga/10127/70600.html');
 
-
-    await page.on('response',async (response) => {
-        if (response.ok()) {
-            if (response.url().indexOf('img') !== -1) {
-                console.log(response.url());
-            }
+    // 监听request事件，筛选一开始加载的图片
+    await page.on('requestfinished', (request) => {
+        if (request.url().indexOf('img') !== -1) {
+            console.log(request.url());
         }
     })
 
-   await page.on('dialog', async dialog => {
-        console.log(dialog.type());
+    // 监听dailog事件，到最后一页就跳转到下页
+    await page.on('dialog', async dialog => {
+        clearInterval(time);
+        await start('https://www.maofly.com/manga/10127/450390_4.html');
     });
 
-    await sleep(5000);
-    await page.click('#all > .next',{
-        clickCount: 5000,
-    });
+    await page.goto(url);
+    await page.content();
 
+    time = setInterval(async () => {
+        await page.click('#all > .next',);
+    }, 5000)
 
+    /*
+
+     // 注入代码
+     await page.addScriptTag({
+         content: `
+                 let mangaDataArr = img_data_arr;
+                 `,
+     })
+     // 获取里面的页数内容
+     const mangaContent = await page.$$eval('div', divs => {
+         return mangaDataArr;
+     })
+
+     await sleep(3000);
+
+ */
 }
 
-start();
+start('https://www.maofly.com/manga/10127/70600.html');
