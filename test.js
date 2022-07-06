11148709456b3d61fa5f63a71803b6ff7a488cf0
xuhang <1009378267@qq.com>
2022-07-06 17:28:49 +0800

    add: 编写测试

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 const puppeteer = require("puppeteer-core");
+const dayjs = require('dayjs');
 const {config} = require("./config");
 const fs = require("fs");
+const fsEX = require('fs-extra');
 const {sleep, createFile, createDir, moveFile,} = require("./util");
 
 const start = async (url) => {
@@ -36,10 +38,56 @@ const start = async (url) => {
 
 }
 
-start('https://www.maofly.com/manga/10127/70600.html');
+// start('https://www.maofly.com/manga/10127/70600.html');
 
 
 // console.log(fs.existsSync('manga'));
+
+
+const sortFilePath = async () => {
+    const files = fs.readdirSync('test');
+// console.log(files);
+    const newFilesPath = [];
+
+    files.map( filePath => {
+        fs.stat(`test/${filePath}`, (err, stats) => {
+            if (err) throw err;
+            let cTime = dayjs(stats.ctime).format('YYYY-MM-DD HH:mm:ss:SSS');
+            newFilesPath.push({
+                cTime,
+                filePath
+            })
+        })
+    })
+    await sleep(2000);
+    return newFilesPath;
+}
+
+
+
+sortFilePath().then( async (res) => {
+    // console.log(res);
+    const newSortFile = res.sort((a, b) => {
+        return new Date(a.cTime) > new Date(b.cTime) ? 1 : -1
+    })
+    // await createDir('test/01');
+    for (let i = 0; i < newSortFile.length; i++) {
+        let len = newSortFile.length;
+        let fileExtension = newSortFile[i].filePath.substring(newSortFile[i].filePath.lastIndexOf('.') + 1);
+        console.log(fileExtension);
+        // await fsEX.copy(`test/${newSortFile[i].filePath}`, `test/01/${i}.${fileExtension}`)
+    }
+    // console.log(newSortFile);
+})
+
+
+
+
+
+
+
+
+
 
 
 
